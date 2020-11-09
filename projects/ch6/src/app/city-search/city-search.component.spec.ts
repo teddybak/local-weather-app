cc17fcdc75738b3e7295064ed80cd6b2f0c67f49
tedybak <tedybak@hotmail.com>
2020-11-09 21:18:12 +0000

    firt commit

---
+++ projects/ch6/src/app/city-search/city-search.component.spec.ts
@@ -0,0 +1,52 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import {
+  ObservablePropertyStrategy,
+  autoSpyObj,
+  injectSpy,
+} from 'angular-unit-test-helper'
+import { of } from 'rxjs'
+
+import { MaterialModule } from '../material.module'
+import { WeatherService } from '../weather/weather.service'
+import { CitySearchComponent } from './city-search.component'
+
+describe('CitySearchComponent', () => {
+  let component: CitySearchComponent
+  let fixture: ComponentFixture<CitySearchComponent>
+
+  let weatherServiceMock: jasmine.SpyObj<WeatherService>
+
+  beforeEach(async(() => {
+    const weatherServiceSpy = autoSpyObj(
+      WeatherService,
+      ['currentWeather$'],
+      ObservablePropertyStrategy.BehaviorSubject
+    )
+
+    TestBed.configureTestingModule({
+      declarations: [CitySearchComponent],
+      imports: [MaterialModule, FormsModule, ReactiveFormsModule, NoopAnimationsModule],
+      providers: [{ provide: WeatherService, useValue: weatherServiceSpy }],
+    }).compileComponents()
+
+    weatherServiceMock = injectSpy(WeatherService)
+  }))
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CitySearchComponent)
+    component = fixture.componentInstance
+  })
+
+  it('should create', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of())
+
+    // Act
+    fixture.detectChanges() // triggers ngOnInit
+
+    // Assert
+    expect(component).toBeTruthy()
+  })
+})
---
+++ src/app/city-search/city-search.component.spec.ts
@@ -0,0 +1,61 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { Store } from '@ngrx/store'
+import {
+  ObservablePropertyStrategy,
+  autoSpyObj,
+  injectSpy,
+} from 'angular-unit-test-helper'
+import { of } from 'rxjs'
+
+import { AppMaterialModule } from '../app-material.module'
+import { WeatherService } from '../weather/weather.service'
+import { CitySearchComponent } from './city-search.component'
+
+describe('CitySearchComponent', () => {
+  let component: CitySearchComponent
+  let fixture: ComponentFixture<CitySearchComponent>
+
+  let weatherServiceMock: jasmine.SpyObj<WeatherService>
+
+  beforeEach(async(() => {
+    const weatherServiceSpy = autoSpyObj(
+      WeatherService,
+      ['currentWeather$'],
+      ObservablePropertyStrategy.BehaviorSubject
+    )
+
+    TestBed.configureTestingModule({
+      declarations: [CitySearchComponent],
+      imports: [
+        AppMaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NoopAnimationsModule,
+      ],
+      providers: [
+        { provide: WeatherService, useValue: weatherServiceSpy },
+        { provide: Store, useValue: null },
+      ],
+    }).compileComponents()
+
+    weatherServiceMock = injectSpy(WeatherService)
+  }))
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CitySearchComponent)
+    component = fixture.componentInstance
+  })
+
+  it('should create', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of())
+
+    // Act
+    fixture.detectChanges() // triggers ngOnInit
+
+    // Assert
+    expect(component).toBeTruthy()
+  })
+})
